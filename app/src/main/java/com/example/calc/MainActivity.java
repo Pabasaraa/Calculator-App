0c0c1268d8158bc8022c2033a34bebd015aef19a
Pabasaraa <pabasara.was@gmail.com>
2022-01-17 16:57:15 +0530

    Text View update when press the buttons & made it fits all screens

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     Button btnDel, btnAns, btnMulti, btnDivide, btnMinus, btnSum, btnRemainder;
     Button num0, num1, num2, num3, num4, num5, num6, num7, num8, num9, dot;
     TextView display;
+    float n1, n2;
+    boolean add, multi, divide, minus, remainder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +40,83 @@ public class MainActivity extends AppCompatActivity {
         dot = findViewById(R.id.btnDot);
 
         display = findViewById(R.id.display);
+
+        num0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "0");
+            }
+        });
+        num1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "1");
+            }
+        });
+        num2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "2");
+            }
+        });
+        num3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "3");
+            }
+        });
+        num4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "4");
+            }
+        });
+        num5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "5");
+            }
+        });
+        num6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "6");
+            }
+        });
+        num7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "7");
+            }
+        });
+        num8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "8");
+            }
+        });
+        num9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "9");
+            }
+        });
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (display.getText().length() != 0){
+                    //do not print the dot
+                }else {
+                    display.setText(display.getText() + ".");
+                }
+            }
+        });
+
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(null);
+            }
+        });
     }
 }
